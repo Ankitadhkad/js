7c5442fe5d7677b0806fd03617e610f186eaa7c3
Ankitadhkad <161940018+Ankitadhkad@users.noreply.github.com>
2024-10-17 11:42:16 +0000

    datatype

---
+++ 01.basics/datatype.js
@@ -0,0 +1,16 @@
+let name="ankita"
+let age =22;
+let login =false;
+//number 
+//bigint 
+//string 
+//boolean
+//nul =>standalone value 
+//undefine
+//symbol
+
+//object 
+
+console.log(typeof "ankita")
+console.log(typeof null)//object 
+
